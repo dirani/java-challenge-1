c7d82371d2ce7c6400fd824fe90d61c10bd427f9
Ricardo Dirani <dirani@gmail.com>
2023-07-28 09:06:29 -0300

    java-challenge-1 and SpringBoot_user
    java-challenge-1 from chatgpt proposed solution
    springboot_user from https://www.youtube.com/watch?v=tWX6SeHdv40

---
+++ java-challenge-1/src/test/java/com/example/javachallenge1/JavaChallenge1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javachallenge1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChallenge1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
