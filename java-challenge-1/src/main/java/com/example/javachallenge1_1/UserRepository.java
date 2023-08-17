0fca487cad1b5b6ee7e32ede8d6357d34a6d1045
Ricardo Dirani <dirani@gmail.com>
2023-08-17 12:30:56 -0300

    Removing scaffolding

--- java-challenge-1/src/main/java/com/example/javachallenge1_1/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.javachallenge1_1;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- nonrest/src/main/java/payroll/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package payroll;
-
-public class OrderNotFoundException extends RuntimeException {
-    public OrderNotFoundException(Long id) {
-        super("Could not find order " + id);
-    }
-}
